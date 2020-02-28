819b7a894415dda62c915275ced571098d10fce2
Everardus Tol <43673344+jobtolmop@users.noreply.github.com>
2020-02-28 09:34:27 +0100

    Add files via upload

---
+++ HealthText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthText : MonoBehaviour
+{
+   [SerializeField] private Text text;
+   [SerializeField] private Player player;
+
+   private void Update()
+   {
+        text.text = "Health = " + player.Health;    
+   }
+
+
+}
